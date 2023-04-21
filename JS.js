d9dd5eaff5ee204ec3988f5d818f0bd83199aa4f
franciscogomez7 <111688611+franciscogomez7@users.noreply.github.com>
2023-04-21 18:55:54 +0200

    Add files via upload

--- JS.js
+++ JS.js
@@ -1,15 +1,19 @@
 const preguntas = [
     {
-      pregunta: "¿Cuál es la capital de España?",
-      respuesta: "Madrid"
+      pregunta: "Tengo sucursal en la esquina, para ahorrar soy la mejor, si necesitas un préstamo, conmigo tienes el mejor trato y honor.",
+      respuesta: "Bankia"
     },
     {
-      pregunta: "¿En qué año se fundó Apple?",
-      respuesta: "1976"
+      pregunta: "Me construyeron los romanos, con la fuerza de sus manos, mi estructura es majestuosa, y hasta hoy, sigue orgullosa.",
+      respuesta: "Acueducto de Segovia"
     },
     {
-      pregunta: "¿Quién escribió 'Cien años de soledad'?",
-      respuesta: "Gabriel García Márquez"
+      pregunta: "Soy un espacio verde, en la ciudad de Segovia, un lugar tranquilo y sereno, donde se respira aire puro.",
+      respuesta: "Parque de la Hontanilla"
+    },
+    {
+      pregunta: "Mis edificios de colores, son un espectáculo visual, en mi centro sen encuentra un zócalo, que es donde la gente suele socializar.",
+      respuesta: "Plaza Mayor"
     }
   ];
   
@@ -34,7 +38,7 @@ const preguntas = [
     preguntaActual++;
     if (preguntaActual >= preguntas.length) {
       document.getElementById("pregunta").style.display = "none";
-      document.getElementById("resultado").innerText = "¡Felicidades, has completado todas las preguntas!";
+      document.getElementById("resultado").innerText = "¡Felicidades, has completado nuestro reto! Ahora dirígete a La Previa donde te esperan nuestros embajadores de marca.";
       document.getElementById("resultado").style.display = "block";
     } else {
       document.getElementById("pregunta").getElementsByTagName("p")[0].innerText = preguntas[preguntaActual].pregunta;
